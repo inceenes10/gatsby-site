86a49a502e09877d8795c55d6eb90e07595f4881
System Administrator <root@Enes-MacBook-Air.local>
2020-10-21 01:41:06 +0300

    aws-exports.js added

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+﻿const awsconfig =  {
+    "aws_appsync_graphqlEndpoint": process.env.APPSYNC_API_URL,
+    "aws_appsync_region": process.env.APPSYNC_REGION,
+    "aws_appsync_authenticationType": process.env.APPSYNC_AUTHENTICATION_TYPE,
+    "aws_appsync_apiKey": process.env.APPSYNC_API_KEY,
+};
+
+export default awsconfig;
